d253c5d8ea60603494ebc7cf4f755ebe5074dc4f
paparazzi2324 <137965879+paparazzi2324@users.noreply.github.com>
2023-12-21 23:29:47 +0100

    Update page.jsx

--- app/(back-office)/dashboard/home/overview/page.jsx
+++ app/(back-office)/dashboard/home/overview/page.jsx
@@ -1,17 +1,13 @@
-// ./app/(back-office)/dashboard/home/overview/page.jsx
-// @ts-nocheck
-// @ts-ignore
-// @jsxImportSource client
-
-import React from 'react';
+import React from 'react'
 import DashboardBanner from '@/components/dashboard/DashboardBanner';
 import SalesOverview from '@/components/dashboard/SalesOverview';
 
-export default function OverviewPage() {
+export default function Dashboard() {
   return (
     <div>
       <DashboardBanner/>
       <SalesOverview/>
     </div>
-  );
+  )
 }
+
