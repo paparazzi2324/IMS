79a8467f12550f7886ddf1baf2b8fdb3f59ab3ec
paparazzi2324 <137965879+paparazzi2324@users.noreply.github.com>
2023-12-21 23:57:10 +0100

    Create ParentComponent.jsx

---
+++ components/dashboard/ParentComponent.jsx
@@ -0,0 +1,13 @@
+/* "use client" */
+import React from 'react';
+import DashboardBanner from './DashboardBanner';
+
+export default function ParentComponent() {
+    // Your component logic here
+    return (
+        <div>
+            <DashboardBanner />
+            {/* Other components or content */}
+        </div>
+    );
+}
