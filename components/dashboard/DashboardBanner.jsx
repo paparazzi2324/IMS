43dd06271502022fab188a632492b1ef8ca119e8
paparazzi2324 <137965879+paparazzi2324@users.noreply.github.com>
2023-12-22 00:06:35 +0100

    Update DashboardBanner.jsx

--- components/dashboard/DashboardBanner.jsx
+++ components/dashboard/DashboardBanner.jsx
@@ -1,11 +1,9 @@
 /* "use client" */
 import { CreditCard, X } from 'lucide-react';
-import React, { useState } from 'react';
 
 export default function DashboardBanner() {
-    const [hidden, setHidden] = useState(false);
     return (
-        <div className={`${hidden ? 'hidden' : 'grid grid-cols-12 items-center gap-5 py-6 px-16 bg-white gap-4 relative'}`}>
+        <div className="grid grid-cols-12 items-center gap-5 py-6 px-16 bg-white gap-4 relative">
             {/* Icon */}
             <div className="col-span-2">
                 <CreditCard className='w-16 h-14 text-slate-700' />
@@ -14,7 +12,7 @@ export default function DashboardBanner() {
             {/* Text */}
             <div className="col-span-6">
                 <h2 className='font-bold text-2xl'>Start accepting online payments</h2>
-                <p>Businesses are moving towards online payments as they&apos;re easy, secure and fast.
+                <p>Businesses are moving towards online payments as they&apos;re easy, secure, and fast.
                     Try them for your businesses today.</p>
             </div>
 
@@ -24,7 +22,7 @@ export default function DashboardBanner() {
             </div>
 
             {/* Close Button */}
-            <button onClick={() => setHidden(true)} className="button absolute top-4 right-16">
+            <button className="button absolute top-4 right-16">
                 <X className='text-slate-500' />
             </button>
         </div>
