45a9e14ac5276e7b973d0602c8395b70b6f9961e
paparazzi2324 <137965879+paparazzi2324@users.noreply.github.com>
2023-12-21 22:40:23 +0100

    Update SupscriptionCard.jsx

--- components/dashboard/SupscriptionCard.jsx
+++ components/dashboard/SupscriptionCard.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
-export default function SupscriptionCard() {
-  return (
-    <div className="px-1 py-3">
-        <div className='rounded-lg p-3 px-3  bg-slate-900'>
-        <div className="border-b border-slate-600 pb-3">
-        <p className='text-xs border-l-2 border-yellow-500 pl-2'>
-            Your Premium plan's trial expires in {" "} <span className="text-orange-400 text-sm">
-                90 days.</span></p>
+export default function SubscriptionCard() {
+    return (
+        <div className="px-1 py-3">
+            <div className='rounded-lg p-3 px-3 bg-slate-900'>
+                <div className="border-b border-slate-600 pb-3">
+                    <p className='text-xs border-l-2 border-yellow-500 pl-2'>
+                        Your Premium plan&apos;s trial expires in {" "} <span className="text-orange-400 text-sm">
+                            90 days.</span></p>
+                </div>
+                <div className="flex text-xs space-x-1 ">
+                    <button className=' border-r border-slate-600 p-1'>Change Plan</button>
+                    <Link href='#' className='p-1'>Upgrade</Link>
+                </div>
+            </div>
         </div>
-      <div className="flex text-xs space-x-1 ">
-        <button className=' border-r border-slate-600 p-1'>Change Plan</button>
-        <Link className='p-1 ' href='#'>Upgrade</Link>
-      </div>
-    </div>
-    </div>
-  )
+    );
 }
